b921f21ccbf922bffa3c622dd52e6e62200586da
Sibo Zhang <sibozhang@users.noreply.github.com>
2016-11-04 13:42:52 -0500

    Add files via upload
    add Dijkstra

---
+++ SimpleDB_2.10/src/test/TestDijkstraAlgorithm.java
@@ -0,0 +1,67 @@
+package test;
+
+/**
+ * Created by mike on 11/4/16.
+ */
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.Test;
+
+import engine.DijkstraAlgorithm;
+import model.Edge;
+import model.Graph;
+import model.Vertex;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class TestDijkstraAlgorithm {
+
+    private List<Vertex> nodes;
+    private List<Edge> edges;
+
+    @Test
+    public void testExcute() {
+        nodes = new ArrayList<Vertex>();
+        edges = new ArrayList<Edge>();
+        for (int i = 0; i < 11; i++) {
+            Vertex location = new Vertex("Node_" + i, "Node_" + i);
+            nodes.add(location);
+        }
+
+        addLane("Edge_0", 0, 1, 85);
+        addLane("Edge_1", 0, 2, 217);
+        addLane("Edge_2", 0, 4, 173);
+        addLane("Edge_3", 2, 6, 186);
+        addLane("Edge_4", 2, 7, 103);
+        addLane("Edge_5", 3, 7, 183);
+        addLane("Edge_6", 5, 8, 250);
+        addLane("Edge_7", 8, 9, 84);
+        addLane("Edge_8", 7, 9, 167);
+        addLane("Edge_9", 4, 9, 502);
+        addLane("Edge_10", 9, 10, 40);
+        addLane("Edge_11", 1, 10, 600);
+
+        // Lets check from location Loc_1 to Loc_10
+        Graph graph = new Graph(nodes, edges);
+        DijkstraAlgorithm dijkstra = new DijkstraAlgorithm(graph);
+        dijkstra.execute(nodes.get(0));
+        LinkedList<Vertex> path = dijkstra.getPath(nodes.get(10));
+
+        assertNotNull(path);
+        assertTrue(path.size() > 0);
+
+        for (Vertex vertex : path) {
+            System.out.println(vertex);
+        }
+
+    }
+
+    private void addLane(String laneId, int sourceLocNo, int destLocNo,
+                         int duration) {
+        Edge lane = new Edge(laneId,nodes.get(sourceLocNo), nodes.get(destLocNo), duration );
+        edges.add(lane);
+    }
+}
