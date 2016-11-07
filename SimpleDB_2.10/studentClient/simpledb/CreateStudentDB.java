8ef79b64b6b68ab2b0858ebe8f2a749766933f2a
丽萍 刘 <lipingliu@lipingdeMacBook-Pro.local>
2016-11-07 08:51:20 -0600

    fxu11

--- SimpleDB_2.10/studentClient/simpledb/CreateStudentDB.java
+++ SimpleDB_2.10/studentClient/simpledb/CreateStudentDB.java
@@ -2,9 +2,11 @@ import java.sql.*;
 import simpledb.remote.SimpleDriver;
 import java.util.Scanner;
 import java.lang.*;
+import java.util.*;
 
 public class CreateStudentDB {
     public static void main(String[] args) throws Exception{
+        System.out.println("simpledb client booted");
 		Connection conn = null;
 		try {
 			Driver d = new SimpleDriver();
@@ -14,36 +16,38 @@ public class CreateStudentDB {
 			Scanner scan = new Scanner(System.in);
 
 			String qry = scan.nextLine();
-
-			CharSequence cs0 = "SELECT";
-			CharSequence cs1 = "select";
-			while(qry.trim() != "exit()") {
-
-				if(qry.contains(cs0) || qry.contains(cs1)){
-					ResultSet rs = stmt.executeQuery(qry.trim());
-
-					ResultSetMetaData rsmd = rs.getMetaData();
-					int columnsNumber = rsmd.getColumnCount();
+            qry = qry.trim().toLowerCase();
+            
+			while(qry.trim().equals("exit()") != true) {
+                if(qry.substring(0, 6).equals("select")) {
+                        ResultSet rs = stmt.executeQuery(qry);
+                        ResultSetMetaData rsmd = rs.getMetaData();
+                        int columnsNumber = rsmd.getColumnCount();
 					// Step 3: loop through the result set
-					System.out.println(columnsNumber);
-					while (rs.next()) {
-						for (int i = 1; i <= columnsNumber; i++) {
-							if (i > 1) System.out.print(",  ");
-							String columnValue = rs.getString(i);
-							System.out.print(columnValue + " " + rsmd.getColumnName(i));
-						}
-						System.out.println("");
-					}
-					
-					rs.close();
-				} else{
-					stmt.executeUpdate(qry.trim());
-					// ResultSet rs = stmt.executeUpdate(qry.trim());
-					
-					// rs.close();
+                        
+                        while (rs.next()) {
+                            for (int i = 1; i <= columnsNumber; i++) {
+                                int currType = rsmd.getColumnType(i);
+                                if (i > 1) System.out.print(",  ");
+                                if(currType==4) {
+                                    int columnValue = rs.getInt(rsmd.getColumnName(i));
+                                    System.out.print(columnValue + " " + rsmd.getColumnName(i));
+                                }
+                                else if(currType==12) {
+                                    String columnValue = rs.getString(rsmd.getColumnName(i));
+                                    System.out.print(columnValue + " " + rsmd.getColumnName(i));
+                                }
+                            }
+                            System.out.println("");
+                        }
+                        
+                }
+				else{
+					stmt.executeUpdate(qry);
 				}
 				qry = scan.nextLine();
-			}
+                qry = qry.trim().toLowerCase();
+            }
 
 		}
 		catch(SQLException e) {
